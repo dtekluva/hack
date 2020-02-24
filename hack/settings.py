c9ed7666cc2539f8d73ac92ba99bce6d1125bedb
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2020-02-24 17:23:41 +0100

    added allowed host

--- hack/settings.py
+++ hack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i9%4(32mwkrtl#rkhlqmf!iqjgo18_a+%o^9zra@wo)&g*itqz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "64.227.114.69"]
 
 
 # Application definition
