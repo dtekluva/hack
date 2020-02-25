0e1caba713df701fa24cb17fcfaf184e9b177779
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2020-02-25 12:58:29 +0100

    modified  static settings

--- hack/settings.py
+++ hack/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
+
+
